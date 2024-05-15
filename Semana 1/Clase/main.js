f6cdde30b23dab1765bce6d0499da545a6234b2c
IgnaDev <arcortoon@gmail.com>
2024-05-14 21:03:56 -0300

    Primer clase

---
+++ Semana 1/Clase/main.js
@@ -0,0 +1,41 @@
+//let edad = 23
+let nombre = "Evil Nacho" 
+let fanDePokemon = false
+
+let tipoDePokemon = typeof fanDePokemon
+
+const URL = "www.google.com"
+
+console.log(typeof 2)
+
+console.log(tipoDePokemon)
+
+let a = 2
+let b = 8
+
+let resultadoSuma = a + b
+let resultadoResta = a - b
+let resultadoMulti = a * b
+let resultadoDiv = a / b
+
+console.log(resultadoSuma)
+console.log(resultadoResta)
+console.log(resultadoMulti)
+console.log(resultadoDiv)
+
+//alert("Hola")
+//let tabien = confirm("¿Todo bien?")
+
+//console.log(tabien)
+
+let edad = prompt("¿Cuantos años tenes?")
+
+console.log(Number(edad))
+
+nombre = prompt("¿Como te llamas?")
+
+console.log(nombre)
+
+let concatTextos = nombre + " " + URL
+
+console.log(concatTextos)
